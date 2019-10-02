72bc1b182593f42763e51664ec0d6108d75a1a3f
Damian Deskiewicz <damian.deskiewicz@softiq.pl>
2019-10-02 15:06:49 +0200

    Post list items

---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PostsRoutingModule } from './posts-routing.module';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+
+
+@NgModule({
+  declarations: [PostListComponent, PostListItemComponent],
+  imports: [
+    CommonModule,
+    PostsRoutingModule
+  ],
+  exports: [
+    CommonModule,
+    PostsRoutingModule,
+    PostListComponent
+  ]
+})
+export class PostsModule { }
