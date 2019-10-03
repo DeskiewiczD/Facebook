53e2f7c2b0017a8ada5c2204906bacb1c8e1cf4a
Damian Deskiewicz <damian.deskiewicz@softiq.pl>
2019-10-03 14:26:10 +0200

    Sortowanie

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+
 import {IPostList} from '../../shared/interfaces/post-list.interface';
+import {sortByOperator} from '../../shared/helpers/sorter.helper';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,12 @@ export class PostsService {
     private http: HttpClient
   ) { }
 
-  getPosts() {
+  async getPosts() {
     const url = "assets/fake-posts.json";
-    return this.http.get<IPostList>(url).toPromise();
+    const response: IPostList = await this.http.get<IPostList>(url)
+      .pipe(
+        sortByOperator('createdTime')
+      ).toPromise();
+    return response;
   }
 }
---
+++ src/app/shared/helpers/sorter.helper.ts
@@ -0,0 +1,23 @@
+import {map} from 'rxjs/operators';
+
+export function sortBy(collection, mapTo) {
+  return collection.sort((item1, item2) => {
+    const date1 = mapTo(item1);
+    const date2 = mapTo(item2);
+    if (date1 < date2) {
+      return 1;
+    } else if (date1 > date2) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+export function sortByOperator(key) {
+  return map((list) => {
+    return sortBy(list, (item) => {
+      return new Date(item[key]);
+    });
+  });
+}
