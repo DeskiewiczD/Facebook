1bcc92296f64046436a660715ad38e75a18fa760
Damian Deskiewicz <damian.deskiewicz@softiq.pl>
2019-10-03 15:42:23 +0200

    Komentarze

---
+++ src/app/comments/components/comment-list/comment-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ICommentList} from '../../../shared/interfaces/comment-list.interface';
+
+@Component({
+  selector: 'app-comment-list',
+  templateUrl: './comment-list.component.html',
+  styleUrls: ['./comment-list.component.scss']
+})
+export class CommentListComponent implements OnInit {
+
+  comments = [
+    {id: 'sdgsgsdgsrwr', body: 'Komentarz 1'},
+    {id: 'sdgsgsdgsrwr', body: 'Komentarz 2'},
+  ] as ICommentList;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
