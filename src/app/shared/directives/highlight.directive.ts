d9fee42d0f98c226e7fede379f6026d9c00bf054
Damian Deskiewicz <damian.deskiewicz@softiq.pl>
2019-10-04 15:47:08 +0200

    Debug

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,8 +1,16 @@
 import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import debug from 'debug';
+
+const console = {
+  log: debug('facebook:highlight:log'),
+  warn: debug('facebook:highlight:warn'),
+  error: debug('facebook:highlight:error')
+};
 
 @Directive({
   selector: '[appHighlight]'
 })
+
 export class HighlightDirective {
 
   constructor(
@@ -11,14 +19,14 @@ export class HighlightDirective {
   ) { }
 
   @HostListener('mouseover') mouseOverHandler() {
-    // console.log('mouseover');
+    console.log('mouseover');
     const $element = this.element.nativeElement;
     this.renderer.addClass($element, 'border');
     this.renderer.addClass($element, 'border-primary');
   }
 
   @HostListener('mouseleave') mouseLeaveHandler() {
-    // console.log('mouseLeave');
+    console.log('mouseLeave');
     const $element = this.element.nativeElement;
     this.renderer.removeClass($element, 'border');
     this.renderer.removeClass($element, 'border-primary');
